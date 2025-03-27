2272f6cdbadf4e60d21e46e40c9f63034f08acd8
Nguyen Kim An <annk.sale@gmail.com>
2025-03-27 16:39:52 +0700

    update config mongodb

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,17 +1,14 @@
-// db/config.ts
+// src/db/config.ts
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/myProjectDB';
+const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+  throw new Error(
+    'Please define the MONGODB_URI environment variable in .env.local'
+  );
 }
 
-/**
- * Global is used here to maintain a cached connection across hot reloads
- * in development. This prevents connections growing exponentially
- * during API Route usage.
- */
 let cached: { conn?: typeof mongoose, promise?: Promise<typeof mongoose> } = (global as any).mongooseCache || {};
 
 async function connectDB() {
@@ -21,12 +18,9 @@ async function connectDB() {
 
   if (!cached.promise) {
     cached.promise = mongoose.connect(MONGODB_URI, {
-      // optional options
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    }).then((mongoose) => {
-      return mongoose;
-    });
+    }).then(mongoose => mongoose);
   }
   cached.conn = await cached.promise;
   (global as any).mongooseCache = cached;
