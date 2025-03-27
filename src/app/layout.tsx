5e4d4a9c617e6b5f22e81034b6390f294d577c17
thanhvanba <vanbatrungthanh@gmail.com>
2025-03-27 15:10:45 +0700

    add test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,20 @@
 import { ConfigProvider } from "antd";
+import { AntdRegistry } from '@ant-design/nextjs-registry';
+import Header from "@/components/header";
+import "./globals.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <ConfigProvider>
-          <div style={{ maxWidth: "800px", margin: "auto", padding: "20px" }}>
-            {children}
-          </div>
-        </ConfigProvider>
+        <AntdRegistry>
+          <ConfigProvider>
+            <div>
+              <Header />
+              {children}
+            </div>
+          </ConfigProvider>
+        </AntdRegistry>
       </body>
     </html>
   );
---
+++ src/components/header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { HomeOutlined, UserOutlined } from '@ant-design/icons';
+
+export default function Header() {
+    return (
+        <div className='flex'>
+            <div className=''>
+                <HomeOutlined /> Home
+            </div>
+            <div className=''>
+                <HomeOutlined /> Home
+            </div>
+            <div className=''>
+                <HomeOutlined /> Home
+            </div>
+        </div>
+    )
+}
